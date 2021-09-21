eb68bf1524c9d7f7b70750e44cc79ca459d2051a
root <hiroki.yamato@icloud.comgit config --global user.name ogura-hirokigit config --global user .email hiroki.yamato@icloud.comhhhhhnanikoregit config --global user.email hiroki.yamato@icloud.com>
2021-09-21 16:51:25 +0000

    error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,14 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in @User
-      redirect_to_@user
-      
+      log_in user
+      redirect_to user
     else
       # ToDo: エラーメッセージを作成する
       render 'new'
     end
   end
-  
+
   def destroy
     log_out
     redirect_to root_url
