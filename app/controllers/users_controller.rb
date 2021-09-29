5be73fd3f007e55c91c5b6b1994f37881bdf253a
ogura-hiroki <hiroki.yamato@icloud.com>
2021-09-29 12:29:07 +0000

    login and logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,24 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)  
     if @user.save
+      log_in @user
       redirect_to @user
     else
       render 'new'
    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
