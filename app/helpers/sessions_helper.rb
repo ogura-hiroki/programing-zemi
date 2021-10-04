c32a12d2752b83dee8304f495e91d6a4681e498f
ogura-hiroki <hiroi.yamato@icloud.com>
2021-10-04 14:56:35 +0000

    10042356

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,23 +1,19 @@
 module SessionsHelper
-    #渡されたユーザーでログインする
+    # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
 
-  def current_user?(user)
-    user == current_user
-  end
-  
   def current_user
     if session[:user_id]
-         if @current_user.nil?
-             @current_user = User.find_by(id: session[:user_id])
-         else
-             @current_user
-         end
+        if @current_user.nil?
+            @current_user = User.find_by(id: session[:user_id])
+        else
+            @current_user
+        end
     end
   end
-    
+
   def logged_in?
     !current_user.nil?
   end
