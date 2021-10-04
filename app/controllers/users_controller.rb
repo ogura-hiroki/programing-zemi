0a54643bd0366faac60f472a45cf24f0d4641707
ogura-hiroki <hiroi.yamato@icloud.com>
2021-10-04 14:37:40 +0000

    user update and authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,6 +66,13 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end
+
     # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
